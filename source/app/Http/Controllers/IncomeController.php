ec2748b0e5edd44cedf6c34a0379eeb14df3521b
Robi Gunawan <inirobi33@gmail.com>
2020-05-13 09:04:08 +0700

    add auth construct

--- source/app/Http/Controllers/IncomeController.php
+++ source/app/Http/Controllers/IncomeController.php
@@ -13,6 +13,10 @@ use Storage;
 
 class IncomeController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
