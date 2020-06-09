2e755a164186156ecd0f0b6b842111508879552b
Deri Lukmana <adaderidisini@gmail.com>
2020-06-09 16:38:57 +0700

    fixing rekap tunggakan siswa

--- source/resources/views/export/rekap_tunggakan_siswa.blade.php
+++ source/resources/views/export/rekap_tunggakan_siswa.blade.php
@@ -23,10 +23,15 @@
         <th data-field="sisa"><div style="text-align:center;">Sisa</div></th>
     </tr>
     @php
+        $i = -1;
+        $d = $datas->count();
         $total = [0,0,0,0];
+        $total_perorang = [0,0,0,0];
     @endphp
     @foreach($datas as $data)
     @php
+        $i++;
+        
         $bulan_spp = 36;
         if ($data->nama == 'SPP')
         {
@@ -42,31 +47,65 @@
         }
         $sisa = $besaran - ( $terbayar + $potongan );
         
+        $cek = false;
+        $cek2 = false;
+
+        $total_perorang[0] += (int) $besaran;
+        $total_perorang[1] += (int) $potongan;
+        $total_perorang[2] += (int) $terbayar;
+        $total_perorang[3] += (int) $sisa;
+            
+
+        try {            
+            if ($datas[$i+1]) {
+                $cek = true;
+                if ($datas[$i]->nama_murid != $datas[$i+1]->nama_murid) {
+                    $cek2 = true;
+                }
+            }
+        } catch (Exception $e) {
+            // exception is raised and it'll be handled here
+            // $e->getMessage() contains the error message
+        }
+
         $total[0] += (int) $besaran;
         $total[1] += (int) $potongan;
         $total[2] += (int) $terbayar;
         $total[3] += (int) $sisa;
     @endphp
+    <tr>
+        <td>{{$no++}}</td>
+        <td >{{$data->nama_murid}}</td>
+        <td ><div style="text-align:center;">{{$data->kelas}}</div></td>
+        <td ><div style="text-align:center;">{{$data->inisial}}</div></td>
+        <td ><div style="text-align:center;">{{$data->angkatan}} ({{$data->tahun_angkatan}})</div></td>
+        <td ><div style="text-align:center;">{{$data->nama}}</div></td>
+        <td ><div style="text-align:right;">{{number_format($besaran,0,',','.')}}</div></td>
+        <td ><div style="text-align:right;">{{number_format($potongan,0,',','.')}}</div></td>
+        <td ><div style="text-align:right;">{{number_format($terbayar,0,',','.')}}</div></td>
+        <td ><div style="text-align:right;">{{number_format($sisa,0,',','.')}}</div></td>
+    </tr>
+    @if ($cek2 || $d == ( $i+1 ))
         <tr>
-            <td>{{$no++}}</td>
-            <td >{{$data->nama_murid}}</td>
-            <td ><div style="text-align:center;">{{$data->kelas}}</div></td>
-            <td ><div style="text-align:center;">{{$data->inisial}}</div></td>
-            <td ><div style="text-align:center;">{{$data->angkatan}} ({{$data->tahun_angkatan}})</div></td>
-            <td ><div style="text-align:center;">{{$data->nama}}</div></td>
-            <td ><div style="text-align:right;">{{number_format($besaran,0,',','.')}}</div></td>
-            <td ><div style="text-align:right;">{{number_format($potongan,0,',','.')}}</div></td>
-            <td ><div style="text-align:right;">{{number_format($terbayar,0,',','.')}}</div></td>
-            <td ><div style="text-align:right;">{{number_format($sisa,0,',','.')}}</div></td>
+            <td colspan="6"> <div style="font-weight:bold; color:red;">Tunggakan {{$data->nama_murid}} : </div> </td>
+            <td ><div style="text-align:right;font-weight:bold; color:red;">{{number_format($total_perorang[0],0,',','.')}}</div></td>
+            <td ><div style="text-align:right;font-weight:bold; color:red;">{{number_format($total_perorang[1],0,',','.')}}</div></td>
+            <td ><div style="text-align:right;font-weight:bold; color:red;">{{number_format($total_perorang[2],0,',','.')}}</div></td>
+            <td ><div style="text-align:right;font-weight:bold; color:red;">{{number_format($total_perorang[3],0,',','.')}}</div></td>
         </tr>
+    @endif
+    @php
+        if ($cek2) {
+            $total_perorang = [0,0,0,0];
+        }
+    @endphp
     @endforeach
-    <tr class="footer-section">
-        <th colspan="6" style="text-align:center"><span style="font-size:20px;font-weight:bold;">Total </span></th>
+    <tr >
+        <th colspan="6" style="text-align:center;"><span style="font-size:12pt;font-weight:bold;">Total </span></th>
         <th style="text-align:right;font-size:12pt;font-weight:bold;">{{number_format($total[0],0,',','.')}}</th>
         <th style="text-align:right;font-size:12pt;font-weight:bold;">{{number_format($total[1],0,',','.')}}</th>
         <th style="text-align:right;font-size:12pt;font-weight:bold;">{{number_format($total[2],0,',','.')}}</th>
         <th style="text-align:right;font-size:12pt;font-weight:bold;">{{number_format($total[3],0,',','.')}}</th>
-        <th>&nbsp;</th>
     </tr>
 </table>
 <small><span style="font-style:italic">Dicetak pada {{now()}}</span></small>
